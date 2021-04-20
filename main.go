fa6be9a7aa27cf3337c0cf269da120eb9194bd53
Harvey Lin <>
2021-04-20 01:51:00 +0000

    Another git test

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
     log.Printf("Opening SQLite file %s\n", DATABASE_PATH)
-    temp, err := sql.Open("sqlite3", DATABASE_PATH)
+    tempDB, err := sql.Open("sqlite3", DATABASE_PATH)
     if err != nil {
         log.Printf("main.go: failed to open database: %v\n", err)
         return
     }
-    db := Database{temp}
+    db := Database{tempDB}
     defer db.Close()
     log.Printf("Successfully opened database %s\n", DATABASE_PATH)
 }
